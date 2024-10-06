3873bab4669e459b470bfc4a39a0e47061907847
Luan Carlos Dantas dos Santos <109487706+Luan-Carlos-Dantas@users.noreply.github.com>
2024-10-06 19:39:02 -0300

    initial commit

---
+++ js-rocket/README.md
@@ -0,0 +1,3 @@
+# Javascript
+  ## Comentários
+  `//comentário de uma única linha` ou `/*comentário de multíplas linhas*/`
---
+++ js-rocket/classroom/scripts.js
@@ -0,0 +1,4 @@
+const app = document.querySelector('#app')
+const h1 = document.createElement('h1')
+h1.innerText = 'Hello World'
+app.append(h1)
